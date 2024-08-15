5e47fc006efe14aa391773d3e1a50bccb05cf708
Lee <>
2024-08-15 21:29:45 +0700

    result

--- categorize-values.py
+++ categorize-values.py
@@ -1,3 +1,12 @@
 myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
 for item in myMixedTypeList:
     print("{} is of the data type {}".format(item,type(item)))
+
+# result
+# ---
+# 45 is of the data type <class 'int'>
+# 290578 is of the data type <class 'int'>
+# 1.02 is of the data type <class 'float'>
+# True is of the data type <class 'bool'>
+# My dog is on the bed. is of the data type <class 'str'>
+# 45 is of the data type <class 'str'>
